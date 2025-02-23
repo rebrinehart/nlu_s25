ad396bc5e63748d9088e8991d5eb8580c2f98333
rebrinehart <135187192+rebrinehart@users.noreply.github.com>
2025-02-23 18:25:34 -0500

    add check to skip analogies for out-of-vocab words

--- hw1/test_analogies.py
+++ hw1/test_analogies.py
@@ -104,6 +104,7 @@ def run_analogy_test(embeddings: Embeddings, test_data: AnalogiesDataset,
     for relation_type, analogies in test_data.items():
         correct = 0
         for a, b, c, d in analogies:
+            if not all(word in embeddings for word in [a, b, c, d]): continue
             target_vec = embeddings[b] - embeddings[a] + embeddings[c]
             closest_words = get_closest_words(embeddings, target_vec.reshape(1, -1), k)[0]
             correct += d in closest_words
